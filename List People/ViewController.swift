becd30cc194da754bf8381dda643ce1220f17a45
Engin Yıldız <engin.yildiz@icterra.com>
2021-10-04 12:43:38 +0300

    Initial Commit

---
+++ List People/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  List People
+//
+//  Created by Engin Yildiz on 4.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
